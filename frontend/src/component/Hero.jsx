ba035133611ff9aab464cfe69e5ced061955089e
Anshcodeify45 <anishpatnaik45@gmail.com>
2026-05-28 13:17:59 +0530

    resume

--- frontend/src/component/Hero.jsx
+++ frontend/src/component/Hero.jsx
@@ -329,7 +329,7 @@ export default function Hero() {
 
           {/* SECONDARY BUTTON */}
           <a
-            href="/Anish_Patnaik.pdf"
+            href="/ANISH_PATNAIK_RESUME.pdf"
             download="Anish_Patnaik_Resume.pdf"
             className="
               group
